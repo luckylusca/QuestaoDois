71f0402a515436fbab4b141d9ba8fbff9978086f
luckylusca <75092912+luckylusca@users.noreply.github.com>
2020-11-27 15:42:39 -0300

    Mudação pra leitura de gênero

--- QuestaoDois.c
+++ QuestaoDois.c
@@ -13,10 +13,11 @@ void copia(FILE *file1,FILE *file2);
 void cadastrar(FILE *file1, FILE *file2);
 void visualizar( FILE *file2);
 
+FILE *file1, *file2;
+
 void main(){
     setlocale(LC_ALL, "portuguese");
     int opcao;
-    FILE *file1, *file2;
     do{
         printf("-----MENU-----\n 1 - registrar\n 2 - visualizar\n 3 - sair\n\nEscolha uma opção: ");
         scanf("%d",&opcao);
@@ -27,7 +28,7 @@ void main(){
                 cadastrar(file1,file2);
                 break;
             case 2:
-
+                visualizar(file2);
                 break;
             case 3:
                 printf("Saindo...");
@@ -36,6 +37,7 @@ void main(){
                 printf("Escolha uma opção valída!");
                 break;
         }
+        system("pause");
         system("cls");
     }while(opcao != 3);
 }
@@ -47,7 +49,7 @@ void cadastrar(FILE *file1,FILE *file2){
         printf("Nome: ");
         scanf("%s",&dado.nome);
 
-        printf("Sexo (m/f): ");
+        printf("Sexo (M/F): ");
         scanf("%s",&dado.sexo);
 
         printf("Idade: ");
@@ -66,24 +68,27 @@ void cadastrar(FILE *file1,FILE *file2){
 }
 
 void copia(FILE *file1,FILE *file2){
-    int f = 0, m = 0, id = 0;
-    char ler[1000], *result;
+    int f = 0, m = 0;
+    char result, ler[3];
     while(!feof(file1)){
-            result = fgets(ler, 1000, file1);
-            if(result == "m"){
-                m++;
-            }else if(result == "f"){
-                f++;
-            }
+        result = fgetc(file1);
+        if(result == 'M' || result == 'm'){
+            m++;
+        }else if(result == 'F' || result == 'f'){
+            f++;
         }
-    fprintf(file2,"O numero de homens: %d \n",m);
+    }
+    fprintf(file2," O numero de homens: %d\n",m);
     fprintf(file2,"O numero de mulheres: %d",f);
 }
 
 void visualizar(FILE *file2){
+    char ler[1000], *result;
     file2 = fopen("saida.txt", "rt");
         while(!feof(file2)){
-
-        }
+            result = fgets(ler,1000,file2);
+            printf("%s ",result);
+    }
+    printf("\n\n");
     fclose(file2);
 }
